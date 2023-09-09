93beeb26b93ec53cd4906bf029f6541ee9fbc666
Dogan Berk BULUR <doganbrkblr@icloud.com>
2023-09-09 22:24:01 +0300

    Update ICategoryRepository.cs

--- RealEstate_API_Dapper/Repositories/CategoryRepository/ICategoryRepository.cs
+++ RealEstate_API_Dapper/Repositories/CategoryRepository/ICategoryRepository.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using RealEstate_API_Dapper.Dtos.CategoryDtos;
+
 namespace RealEstate_API_Dapper.Repositories.CategoryRepository
 {
 	public interface ICategoryRepository
 	{
+		Task<List<ResultCategoryDto>> GetAllCategoryAsync();
 	}
 }
 
